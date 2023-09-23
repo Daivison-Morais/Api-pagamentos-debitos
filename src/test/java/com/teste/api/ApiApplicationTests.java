c2276bbb3cb9ff0c8c4d650e01c2f23c9133cae0
Daivison-Morais <daivison13pinheiro@gmail.com>
2023-09-23 12:46:20 -0300

    commit inicial

---
+++ src/test/java/com/teste/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
